e2322815745621c1731989e039c2dfb1d175fa3c
FatihKoseoglu <koseoglufatih@gmail.com>
2024-07-03 14:29:06 +0300

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+
+namespace UdemyRedisCalisma.Controllers
+{
+    public class ProductController : Controller
+    {
+        readonly IMemoryCache _memoryCache;
+
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            _memoryCache.Set<string>("zaman",DateTime.Now.ToString());
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            ViewBag.zaman=_memoryCache.Get<string>("zaman");
+            return View();
+        }
+    }
+}
